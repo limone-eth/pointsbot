15d8a7ea9442f03f86b441b2df7285667683b998
lim0n3 <simonestaffa96@gmail.com>
2023-12-19 10:20:35 +0100

    add support for transfer events

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
+import "dotenv/config";
 import express from "express";
 import {prepareAlchemyData, validateAlchemySignature} from "./utils/alchemy";
-import {processPoolSwapEvent} from "./controllers/webhooks";
+import {processPoolSwapEvent} from "./controllers/webhooks/swap";
 import cron from "node-cron";
 
-import "dotenv/config";
-import {publishFarcasterLeaderboard} from "./utils/airstack/functions/fetch-token-holders";
+import {publishTokenStats} from "./jobs/token-stats";
+import {publishFarcasterLeaderboard} from "./jobs/farcaster-holders-leaderboard";
+import {processTransferEvent} from "./controllers/webhooks/transfer";
 
 // init express app
 export const app = express();
@@ -12,38 +14,38 @@ export const app = express();
 app.use(express.json({limit: "10mb"}));
 
 app.post(
-  "/webhooks/points",
+  "/webhooks/token/swaps",
   // Middlewares needed to validate the alchemy signature
   prepareAlchemyData(),
   validateAlchemySignature(process.env.ALCHEMY_WEBHOOK_SIGNING_KEY_SWAP),
   processPoolSwapEvent
 );
 
-/*app.post(
-  "/webhooks/points/transfers",
+app.post(
+  "/webhooks/token/transfers",
   // Middlewares needed to validate the alchemy signature
   prepareAlchemyData(),
   validateAlchemySignature(process.env.ALCHEMY_WEBHOOK_SIGNING_KEY_TRANSFER),
   processTransferEvent
-);*/
+);
 
 app.listen(process.env.PORT || 3000, () => {
   console.log("Server started on port 3000");
 });
 
 // run every 1 hour
-/*cron.schedule("0 * * * *", async () => {
-  console.log("Elaborating Point stats...");
+cron.schedule("0 */3 * * *", async () => {
+  console.log("Elaborating Tokens stats...");
   try {
-    await publishPointsStats();
+    await publishTokenStats();
   } catch (e) {
     console.error(e);
-    await publishPointsStats();
+    await publishTokenStats();
   }
-});*/
+});
 
 // run every hour at 30 minutes
-cron.schedule("30 */8 * * *", async () => {
+cron.schedule("30 */6 * * *", async () => {
   console.log("Elaborating Farcaster leaderboard");
   try {
     await publishFarcasterLeaderboard(10);
